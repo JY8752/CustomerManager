fbba7856e8120899f194274e7eefd59cc8453ad1
JY8752e <j.y.8752@gmail.com>
2019-11-20 21:06:49 +0000

    JavaScript const delete

--- app/assets/javascripts/houses.js
+++ app/assets/javascripts/houses.js
@@ -19,9 +19,9 @@
 // });
 
 document.addEventListener('turbolinks:load', function() {
-  const apartments = document.getElementsByClassName('apartment');
-  const radio_s = document.getElementById('house_style_戸建');
-  const radio_m = document.getElementById('house_style_集合住宅');
+  var apartments = document.getElementsByClassName('apartment');
+  var radio_s = document.getElementById('house_style_戸建');
+  var radio_m = document.getElementById('house_style_集合住宅');
 
   function radio() {
     if(radio_s.checked) {
